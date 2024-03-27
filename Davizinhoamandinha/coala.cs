9e2c8906ddfb435cd52bcca29fd37e99d5d43b8b
davi camacho <davicamachom@gmail.com>
2024-03-27 09:53:07 -0300

    continuo e colocou as imagem

--- Davizinhoamandinha/coala.cs
+++ Davizinhoamandinha/coala.cs
@@ -1,6 +1,10 @@
 namespace Davizinhoamandinha;
-public class coala : paianimal
+public class coala : paianimal;
+  
+  public coala()
 {
+  InitializeComponent();
 
-    
-}
+
+  var a = new paianimal();
+  a.felicidade = 60;
--- quizdavi/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace quizdavi;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
