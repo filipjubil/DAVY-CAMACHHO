20f063a2fee46c0eeec352925415884ecbfb1343
davi <davicamachom@gmail.com>
2024-04-05 09:03:13 -0300

    continuou

--- Davizinhoamandinha/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Davizinhoamandinha.AssemblyInfo.cs
+++ Davizinhoamandinha/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Davizinhoamandinha.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Davizinhoamandinha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+37f5f813a66e704a73e65ea21cfe25918dde2070")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+73bbb8d37435452f5edbab402dec7388e07ccdd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Davizinhoamandinha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Davizinhoamandinha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
